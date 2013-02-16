83e9f7086dd2cb5b2484fdf6fbd663f52a5c50ab
DanyaPostfactum <danya.postfactum@gmail.com>
2013-02-17 01:21:28 +1000

    Validate on uninstall

--- _build/validator.php
+++ _build/validator.php
@@ -1,24 +1,28 @@
 <?php
 /**
  *
- * @author Danil Kostin <danya@postfactum@gmail.com>
+ * @author Danil Kostin <danya.postfactum@gmail.com>
  *
  * @package ajaxmanager
  * @subpackage build
  */
 
 
-    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-        case xPDOTransport::ACTION_INSTALL:
-        case xPDOTransport::ACTION_UPGRADE:
-            $version = $object->xpdo->getVersionData();
-            $object->xpdo->log(xPDO::LOG_LEVEL_ERROR, $version['full_version'] . print_r(version_compare($version['full_version'], '2.5.6', '<'), 1));
-            if (version_compare($version['full_version'], '2.2.6', '<')) {
-                $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'This package requires MODX 2.2.6! Aborting installation...');
-                return false;
-            }
-            break;
-        default:
-    }
+switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+    case xPDOTransport::ACTION_INSTALL:
+    case xPDOTransport::ACTION_UPGRADE:
+        $version = $object->xpdo->getVersionData();
+        if (version_compare($version['full_version'], '2.2.6', '=')) {
+            $object->xpdo->log(xPDO::LOG_LEVEL_ERROR,'This package requires MODX 2.2.6! Aborting installation...');
+            return false;
+        }
+        break;
+    case xPDOTransport::ACTION_UNINSTALL:
+        if ($options[xPDOTransport::PREEXISTING_MODE] != xPDOTransport::RESTORE_PREEXISTING) {
+            $transport->xpdo->log(xPDO::LOG_LEVEL_ERROR, 'Please press Uninstall button again and set “Restore” mode! Aborting...');
+            return false;
+        }
+        break;
+}
 
 return true;
