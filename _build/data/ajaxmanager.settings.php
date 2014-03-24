3b8c67dd90a0a6d302b2949362a0694ca660011d
DanyaPostfactum <danya.postfactum@gmail.com>
2014-03-24 11:58:07 +1000

    Add `resourcehider` to the list of compatible components

--- _build/data/ajaxmanager.settings.php
+++ _build/data/ajaxmanager.settings.php
@@ -5,7 +5,7 @@ $settings['compatible_namespaces']= $modx->newObject('modSystemSetting');
 $settings['compatible_namespaces']->fromArray(array(
         'key' => 'ajaxmanager.compatible_namespaces',
         'xtype' => 'textfield',
-        'value' => 'core,clientconfig,minishop2',
+        'value' => 'core,clientconfig,minishop2,resourcehider',
         'namespace' => 'ajaxmanager',
         'area' => 'general'
     ),'',true,true);
