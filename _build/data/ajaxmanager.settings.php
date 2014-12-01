51f30da074f99521269127304c15ed8a46c0f517
DanyaPostfactum <danya.postfactum@gmail.com>
2014-12-02 00:20:47 +1000

    Add Gallery to the list of supported components

--- _build/data/ajaxmanager.settings.php
+++ _build/data/ajaxmanager.settings.php
@@ -5,7 +5,7 @@ $settings['compatible_namespaces']= $modx->newObject('modSystemSetting');
 $settings['compatible_namespaces']->fromArray(array(
         'key' => 'ajaxmanager.compatible_namespaces',
         'xtype' => 'textfield',
-        'value' => 'core,clientconfig,minishop2,resourcehider',
+        'value' => 'core,clientconfig,minishop2,resourcehider,gallery',
         'namespace' => 'ajaxmanager',
         'area' => 'general'
     ),'',true,true);
